38a06c49c98dc475047cbf5a51c031bd2db015c0
rcorcios023 <56006220+rcorcios023@users.noreply.github.com>
2019-10-30 00:40:14 -0400

    lesson 301

---
+++ src/main/java/com/example/demo/Lesson301AApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lesson301AApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lesson301AApplication.class, args);
+    }
+
+}
